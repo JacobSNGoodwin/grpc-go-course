fbf83c98491a4096c6463fb2098f35a8e1610c87
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-06-13 08:50:04 -0700

    Create boilerplate for greet service

---
+++ greet/greet_server/server.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/maxbrain0/grpc-go-course/greet/greetpb"
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+func main() {
+	fmt.Println("Hello world")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+
+	if err != nil {
+		log.Fatalf("Failed to listen %v", err)
+	}
+
+	s := grpc.NewServer()
+
+	greetpb.RegisterGreetServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
