147761dce5bcfe4b4aecb624275b280bbb4907c0
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-07-08 11:07:58 -0700

    Generate CreateBlog rpc code

--- blog/blogpb/blog.pb.go
+++ blog/blogpb/blog.pb.go
@@ -8,6 +8,8 @@ import (
 	fmt "fmt"
 	proto "github.com/golang/protobuf/proto"
 	grpc "google.golang.org/grpc"
+	codes "google.golang.org/grpc/codes"
+	status "google.golang.org/grpc/status"
 	math "math"
 )
 
@@ -85,24 +87,108 @@ func (m *Blog) GetContent() string {
 	return ""
 }
 
+type CreateBlogRequest struct {
+	Blog                 *Blog    `protobuf:"bytes,1,opt,name=blog,proto3" json:"blog,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *CreateBlogRequest) Reset()         { *m = CreateBlogRequest{} }
+func (m *CreateBlogRequest) String() string { return proto.CompactTextString(m) }
+func (*CreateBlogRequest) ProtoMessage()    {}
+func (*CreateBlogRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_a4b0406114889fe6, []int{1}
+}
+
+func (m *CreateBlogRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_CreateBlogRequest.Unmarshal(m, b)
+}
+func (m *CreateBlogRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_CreateBlogRequest.Marshal(b, m, deterministic)
+}
+func (m *CreateBlogRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_CreateBlogRequest.Merge(m, src)
+}
+func (m *CreateBlogRequest) XXX_Size() int {
+	return xxx_messageInfo_CreateBlogRequest.Size(m)
+}
+func (m *CreateBlogRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_CreateBlogRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_CreateBlogRequest proto.InternalMessageInfo
+
+func (m *CreateBlogRequest) GetBlog() *Blog {
+	if m != nil {
+		return m.Blog
+	}
+	return nil
+}
+
+type CreateBlogResponse struct {
+	Blog                 *Blog    `protobuf:"bytes,1,opt,name=blog,proto3" json:"blog,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *CreateBlogResponse) Reset()         { *m = CreateBlogResponse{} }
+func (m *CreateBlogResponse) String() string { return proto.CompactTextString(m) }
+func (*CreateBlogResponse) ProtoMessage()    {}
+func (*CreateBlogResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_a4b0406114889fe6, []int{2}
+}
+
+func (m *CreateBlogResponse) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_CreateBlogResponse.Unmarshal(m, b)
+}
+func (m *CreateBlogResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_CreateBlogResponse.Marshal(b, m, deterministic)
+}
+func (m *CreateBlogResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_CreateBlogResponse.Merge(m, src)
+}
+func (m *CreateBlogResponse) XXX_Size() int {
+	return xxx_messageInfo_CreateBlogResponse.Size(m)
+}
+func (m *CreateBlogResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_CreateBlogResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_CreateBlogResponse proto.InternalMessageInfo
+
+func (m *CreateBlogResponse) GetBlog() *Blog {
+	if m != nil {
+		return m.Blog
+	}
+	return nil
+}
+
 func init() {
 	proto.RegisterType((*Blog)(nil), "blog.Blog")
+	proto.RegisterType((*CreateBlogRequest)(nil), "blog.CreateBlogRequest")
+	proto.RegisterType((*CreateBlogResponse)(nil), "blog.CreateBlogResponse")
 }
 
 func init() { proto.RegisterFile("blog/blogpb/blog.proto", fileDescriptor_a4b0406114889fe6) }
 
 var fileDescriptor_a4b0406114889fe6 = []byte{
-	// 149 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0x4b, 0xca, 0xc9, 0x4f,
-	0xd7, 0x07, 0x11, 0x05, 0x49, 0x60, 0x4a, 0xaf, 0xa0, 0x28, 0xbf, 0x24, 0x5f, 0x88, 0x05, 0xc4,
-	0x56, 0x4a, 0xe5, 0x62, 0x71, 0xca, 0xc9, 0x4f, 0x17, 0xe2, 0xe3, 0x62, 0xca, 0x4c, 0x91, 0x60,
-	0x54, 0x60, 0xd4, 0xe0, 0x0c, 0x62, 0xca, 0x4c, 0x11, 0x92, 0xe6, 0xe2, 0x4c, 0x2c, 0x2d, 0xc9,
-	0xc8, 0x2f, 0x8a, 0xcf, 0x4c, 0x91, 0x60, 0x02, 0x0b, 0x73, 0x40, 0x04, 0x3c, 0x53, 0x84, 0xc4,
-	0xb8, 0xd8, 0x4a, 0x32, 0x4b, 0x72, 0x52, 0x53, 0x25, 0x98, 0xc1, 0x32, 0x50, 0x9e, 0x90, 0x04,
-	0x17, 0x7b, 0x72, 0x7e, 0x5e, 0x49, 0x6a, 0x5e, 0x89, 0x04, 0x0b, 0x58, 0x02, 0xc6, 0x35, 0xe2,
-	0xe5, 0xe2, 0x06, 0x59, 0x13, 0x9c, 0x5a, 0x54, 0x96, 0x99, 0x9c, 0xea, 0xc4, 0x11, 0xc5, 0x06,
-	0x71, 0x50, 0x12, 0x1b, 0xd8, 0x31, 0xc6, 0x80, 0x00, 0x00, 0x00, 0xff, 0xff, 0xc7, 0x0f, 0x59,
-	0x5e, 0xa6, 0x00, 0x00, 0x00,
+	// 219 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x84, 0x90, 0xc1, 0x4b, 0x85, 0x40,
+	0x10, 0xc6, 0x7b, 0x26, 0xf6, 0xde, 0x08, 0x41, 0x73, 0xb0, 0xa5, 0x20, 0xc2, 0x53, 0x27, 0x03,
+	0xed, 0x1f, 0xc8, 0x4e, 0xdd, 0xc2, 0x6e, 0x5d, 0x42, 0xdd, 0xc1, 0x16, 0xc4, 0xb5, 0x75, 0xec,
+	0xef, 0x8f, 0x9d, 0x2d, 0x0a, 0x3c, 0xbc, 0xcb, 0xee, 0x7e, 0xf3, 0xcd, 0xec, 0xf7, 0x63, 0x20,
+	0xeb, 0x46, 0x3b, 0xdc, 0xfb, 0x63, 0xee, 0xe4, 0x2a, 0x66, 0x67, 0xd9, 0x62, 0xec, 0xdf, 0x39,
+	0x41, 0x5c, 0x8f, 0x76, 0xc0, 0x73, 0x88, 0x8c, 0x56, 0xbb, 0xdb, 0xdd, 0xdd, 0xa1, 0x89, 0x8c,
+	0xc6, 0x6b, 0x38, 0xb4, 0x2b, 0x7f, 0x58, 0xf7, 0x6e, 0xb4, 0x8a, 0xa4, 0xbc, 0x0f, 0x85, 0x67,
+	0x8d, 0x19, 0x24, 0x6c, 0x78, 0x24, 0x52, 0xa7, 0xe2, 0xfc, 0x28, 0x54, 0x70, 0xd6, 0xdb, 0x89,
+	0x69, 0x62, 0x15, 0x8b, 0xf1, 0x2b, 0xf3, 0x0a, 0x2e, 0x9e, 0x1c, 0xb5, 0x4c, 0x3e, 0xac, 0xa1,
+	0xcf, 0x95, 0x16, 0xc6, 0x1b, 0x10, 0x06, 0x49, 0x4d, 0x4b, 0x28, 0x04, 0x4e, 0x1a, 0x02, 0xdb,
+	0x03, 0xe0, 0xff, 0xa1, 0x65, 0xb6, 0xd3, 0x42, 0xc7, 0xa6, 0xca, 0x17, 0x48, 0xbd, 0x7a, 0x25,
+	0xf7, 0x65, 0x7a, 0xc2, 0x47, 0x80, 0xbf, 0x4f, 0xf0, 0x32, 0xb4, 0x6f, 0x58, 0xae, 0xd4, 0xd6,
+	0x08, 0x79, 0xf9, 0x49, 0xbd, 0x7f, 0x4b, 0xc2, 0xfa, 0xba, 0x44, 0x56, 0x57, 0x7d, 0x07, 0x00,
+	0x00, 0xff, 0xff, 0xd8, 0x5c, 0xab, 0x44, 0x54, 0x01, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -117,6 +203,7 @@ const _ = grpc.SupportPackageIsVersion4
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type BlogServiceClient interface {
+	CreateBlog(ctx context.Context, in *CreateBlogRequest, opts ...grpc.CallOption) (*CreateBlogResponse, error)
 }
 
 type blogServiceClient struct {
@@ -127,22 +214,59 @@ func NewBlogServiceClient(cc *grpc.ClientConn) BlogServiceClient {
 	return &blogServiceClient{cc}
 }
 
+func (c *blogServiceClient) CreateBlog(ctx context.Context, in *CreateBlogRequest, opts ...grpc.CallOption) (*CreateBlogResponse, error) {
+	out := new(CreateBlogResponse)
+	err := c.cc.Invoke(ctx, "/blog.BlogService/CreateBlog", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // BlogServiceServer is the server API for BlogService service.
 type BlogServiceServer interface {
+	CreateBlog(context.Context, *CreateBlogRequest) (*CreateBlogResponse, error)
 }
 
 // UnimplementedBlogServiceServer can be embedded to have forward compatible implementations.
 type UnimplementedBlogServiceServer struct {
 }
 
+func (*UnimplementedBlogServiceServer) CreateBlog(ctx context.Context, req *CreateBlogRequest) (*CreateBlogResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method CreateBlog not implemented")
+}
+
 func RegisterBlogServiceServer(s *grpc.Server, srv BlogServiceServer) {
 	s.RegisterService(&_BlogService_serviceDesc, srv)
+}
+
+func _BlogService_CreateBlog_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(CreateBlogRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(BlogServiceServer).CreateBlog(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/blog.BlogService/CreateBlog",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(BlogServiceServer).CreateBlog(ctx, req.(*CreateBlogRequest))
+	}
+	return interceptor(ctx, in, info, handler)
 }
 
 var _BlogService_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "blog.BlogService",
 	HandlerType: (*BlogServiceServer)(nil),
-	Methods:     []grpc.MethodDesc{},
-	Streams:     []grpc.StreamDesc{},
-	Metadata:    "blog/blogpb/blog.proto",
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "CreateBlog",
+			Handler:    _BlogService_CreateBlog_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "blog/blogpb/blog.proto",
 }
