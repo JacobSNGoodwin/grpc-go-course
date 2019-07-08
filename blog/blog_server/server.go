f99f19cd988d2628c271feec70214b318dd9233e
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-07-08 10:18:24 -0700

    Gracecfully terminate server

--- blog/blog_server/server.go
+++ blog/blog_server/server.go
@@ -1,9 +1,11 @@
-package blog_server
+package main
 
 import (
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"os/signal"
 
 	"github.com/maxbrain0/grpc-go-course/blog/blogpb"
 	"google.golang.org/grpc"
@@ -25,7 +27,22 @@ func main() {
 
 	blogpb.RegisterBlogServiceServer(s, &server{})
 
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("Failed to serve: %v", err)
-	}
+	go func() {
+		fmt.Println("Starting Server....")
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("Failed to serve: %v", err)
+		}
+	}()
+
+	// Wait for Control C to exit - Go specific
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+
+	// Block until a signal is received
+	<-ch
+	fmt.Println("Stopping the server")
+	s.Stop() // stopping grpc server
+	fmt.Println("Closing listener")
+	lis.Close()
+	fmt.Println("End of program")
 }
