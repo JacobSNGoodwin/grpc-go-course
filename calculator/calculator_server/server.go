dffba39428ed3db0a669b43c57cbae7f4ca97611
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-07-08 09:26:27 -0700

    Add grpc/reflection

--- calculator/calculator_server/server.go
+++ calculator/calculator_server/server.go
@@ -9,6 +9,7 @@ import (
 	"net"
 
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/reflection"
 	"google.golang.org/grpc/status"
 
 	"github.com/maxbrain0/grpc-go-course/calculator/calculatorpb"
@@ -143,6 +144,9 @@ func main() {
 	s := grpc.NewServer()
 
 	calculatorpb.RegisterCalculatorServiceServer(s, &server{})
+
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
 
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve: %v", err)
